18c9fa17880df94d81bfb2e58e2a6dc8effdea71
Gpx <polvara@gmail.com>
2017-07-04 18:10:09 +0200

    Improve tests for this feature

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 const getTitles = () => {
-  const titles = document.querySelectorAll('h2, h3')
+  const titles = document.querySelectorAll('h2, h3, h4')
   return Array.from(titles)
 }
 
 const setupTOC = () => {
+  const toc = document.getElementById('toc')
+  const titles = getTitles()
+
+  let h2 = 0
+  let h3 = 0
+  let h4 = 0
+  titles.map(title => {
+    let text
+    switch(title.tagName) {
+      case 'H2':
+        h3 = 0
+        h4 = 0
+        text = `${++h2} ${title.textContent}`
+        break
+      case 'H3':
+        h4 = 0
+        text = `${h2}.${++h3} ${title.textContent}`
+        break
+      case 'H4':
+        text = `${h2}.${h3}.${++h4} ${title.textContent}`
+        break
+    }
+    const url = encodeURIComponent(title.textContent)
+
+    title.id = url
+
+    return { text, url }
+  }).map(({ text, url }) => {
+    const titleElement = document.createElement('a')
+    titleElement.href = `#${url}`
+    titleElement.text = text
+    toc.appendChild(titleElement)
+  })
 }
 
 addExternalLinks = () => {
