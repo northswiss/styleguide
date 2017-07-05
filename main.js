6f0df7ea50213bc97b28ce067daf12314520ec48
Gpx <polvara@gmail.com>
2017-07-05 19:43:36 +0200

    Tests and intro

--- main.js
+++ main.js
@@ -12,29 +12,34 @@ const setupTOC = () => {
   let h4 = 0
   titles.map(title => {
     let text
+    let level
     switch(title.tagName) {
       case 'H2':
         h3 = 0
         h4 = 0
         text = `${++h2} ${title.textContent}`
+        level = 2
         break
       case 'H3':
         h4 = 0
         text = `${h2}.${++h3} ${title.textContent}`
+        level = 3
         break
       case 'H4':
         text = `${h2}.${h3}.${++h4} ${title.textContent}`
+        level = 4
         break
     }
     const url = encodeURIComponent(title.textContent)
 
     title.id = url
 
-    return { text, url }
-  }).map(({ text, url }) => {
+    return { text, url, level }
+  }).map(({ text, url, level }) => {
     const titleElement = document.createElement('a')
     titleElement.href = `#${url}`
     titleElement.text = text
+    titleElement.classList.add(`level_${level}`)
     toc.appendChild(titleElement)
   })
 }
